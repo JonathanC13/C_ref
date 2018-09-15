b2e90c8e662a195f237f88e6abe6da03fa92f2d9
JonathanC13 <jonhschan@hotmail.com>
2018-09-14 20:14:35 -0400

    array on shared mem

--- c_exercises/sh2_cons.c
+++ c_exercises/sh2_cons.c
@@ -128,3 +128,50 @@ int main(){
   exit(EXIT_SUCCESS);
 
 }
+
+/*
+Reader from array on shared memory
+
+int main(){
+
+	int arr_size = 5;
+	
+	int *shared_memory_arr;
+  	int shmid;
+
+  	// create a shared memory to fit the struct array.
+  	//int shmget(key_t key, size_t size, int shmflg);
+	shmid = shmget((key_t)1234, sizeof(int) * arr_size, 0666 | IPC_CREAT);	
+	//errno = 0;
+	if(shmid == -1){
+		fprintf(stderr, "shmget failed: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+	
+	// attach
+  	//void *shmat(int shm_id, const void *shm_addr, int shmflg);
+  	shared_memory_arr = shmat(shmid, (void*)0, 0);	//attach the shared memory to shmid. Returns pointer to the first byte of the shared memory
+  	if (shared_memory == (void*)-1){
+   		fprintf(stderr, "shmat failed\n");
+    		exit(EXIT_FAILURE);
+  	}
+
+ 	printf("Memory attached at %X\n", (int)shared_memory_arr);
+	
+	//Read from the array on shared memory
+	printf("Reader reads: \n");
+	for (int i = 0; i < arr_size; i ++){
+		printf("%d \n", shared_memory_arr[i]);
+	}
+	
+	// after finished, detach from shared memory
+	if (shmdt(shared_memory_arr) == -1){
+		fprintf(stderr, "shmdt failed\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	return 0;
+
+}
+
+*/
